26292e0d35f935c416a85037dd9cc7b71b4b9940
sfanxiang <sfanxiang@gmail.com>
2016-06-07 05:25:26 +0800

    test

--- booter/io.c
+++ booter/io.c
@@ -2,6 +2,14 @@
 
 #include "file.h"
 
+char *memcpy2(char *dst, const char *src, size_t size)
+{
+	for (size_t i = 0; i < size; i++) {
+		*(dst + i) = *(src + i);
+	}
+	return dst;
+}
+
 char phy_memcpy_to(size_t pos, const char *data, size_t size)
 {
 	UNICODE_STRING uniName;
@@ -47,7 +55,7 @@ char phy_memcpy_from(char *data, size_t pos, size_t size)
 
 	RtlInitUnicodeString(&uniName, L"\\Device\\PhysicalMemory");
 	InitializeObjectAttributes(&objAttr, &uniName,
-		OBJ_KERNEL_HANDLE | OBJ_FORCE_ACCESS_CHECK, NULL, NULL);
+		OBJ_KERNEL_HANDLE, NULL, NULL);
 
 	HANDLE handle;
 	NTSTATUS ntstatus;
@@ -69,7 +77,7 @@ char phy_memcpy_from(char *data, size_t pos, size_t size)
 
 		size_t shift = max(pos, x); shift = shift - (size_t)PAGE_ALIGN(shift);
 		size_t done = x + shift - pos;
-		memcpy(data + done, mapped + shift, min(PAGE_SIZE, size - done));
+		memcpy2(data + done, mapped + shift, min(PAGE_SIZE, size - done));
 
 		ZwUnmapViewOfSection(ZwCurrentProcess(), mapped);
 	}
@@ -86,8 +94,8 @@ char write_boot_image(char *data, size_t size)
 			PHYSICAL_ADDRESS pa;
 			pa.QuadPart = 0x00000000FFFFFFFF;
 			char *t = MmAllocateContiguousMemory(8192, pa);
-			if (t && phy_memcpy_from(t, 0x6010, 8191)) {
-				write_file(L"\\DosDevices\\C:\\booter_message.txt", 0, t, 8191);
+			if (t && phy_memcpy_from(t, 0x8001, 4094)) {
+				//write_file(L"\\DosDevices\\C:\\booter_message.txt", 0, t, 4094);
 			}
 			return 1;
 		}
