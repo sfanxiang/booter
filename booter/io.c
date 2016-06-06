9c9f1a4796ebb6e1f6595f9b54cd3653163785c3
sfanxiang <sfanxiang@gmail.com>
2016-06-06 23:22:50 +0800

    test

--- booter/io.c
+++ booter/io.c
@@ -13,18 +13,21 @@ char phy_memcpy_to(size_t pos, const char *data, size_t size)
 
 	HANDLE handle;
 	NTSTATUS ntstatus;
-	
+
 	ntstatus = ZwOpenSection(&handle, SECTION_MAP_READ | SECTION_MAP_WRITE, &objAttr);
 	if (!NT_SUCCESS(ntstatus)) return 0;
 
-	LARGE_INTEGER phy; char *mapped;
+	LARGE_INTEGER phy; char *mapped; size_t ps;
 	for (size_t x = (size_t)PAGE_ALIGN(pos); x < pos + size; x += (size_t)PAGE_SIZE) {
 		phy.QuadPart = x;
 		mapped = NULL;
-		size_t ps = PAGE_SIZE;
+		ps = PAGE_SIZE;
 		ntstatus = ZwMapViewOfSection(handle, ZwCurrentProcess(), &mapped, 0,
 			PAGE_SIZE, &phy, &(SIZE_T)ps, ViewUnmap, 0, PAGE_READWRITE);
-		if (!NT_SUCCESS(ntstatus)) return 0;
+		if (!NT_SUCCESS(ntstatus)) {
+			ZwClose(handle);
+			return 0;
+		}
 
 		size_t shift = max(pos, x); shift = shift - (size_t)PAGE_ALIGN(shift);
 		size_t done = x + shift - pos;
@@ -33,6 +36,7 @@ char phy_memcpy_to(size_t pos, const char *data, size_t size)
 		ZwUnmapViewOfSection(ZwCurrentProcess(), mapped);
 	}
 
+	ZwClose(handle);
 	return 1;
 }
 
@@ -48,25 +52,30 @@ char phy_memcpy_from(char *data, size_t pos, size_t size)
 	HANDLE handle;
 	NTSTATUS ntstatus;
 
-	ntstatus = ZwOpenSection(&handle, SECTION_MAP_READ, &objAttr);
+	ntstatus = ZwOpenSection(&handle, SECTION_MAP_READ | SECTION_MAP_WRITE, &objAttr);
 	if (!NT_SUCCESS(ntstatus)) return 0;
 
-	LARGE_INTEGER phy; char *mapped;
+	LARGE_INTEGER phy; char *mapped; size_t ps;
 	for (size_t x = (size_t)PAGE_ALIGN(pos); x < pos + size; x += (size_t)PAGE_SIZE) {
 		phy.QuadPart = x;
 		mapped = NULL;
-		size_t ps = PAGE_SIZE;
+		ps = PAGE_SIZE;
 		ntstatus = ZwMapViewOfSection(handle, ZwCurrentProcess(), &mapped, 0,
 			PAGE_SIZE, &phy, &(SIZE_T)ps, ViewUnmap, 0, PAGE_READONLY);
-		if (!NT_SUCCESS(ntstatus)) return 0;
+		if (!NT_SUCCESS(ntstatus)) {
+			ZwClose(handle);
+			return 0;
+		}
 
-		size_t shift = max(pos, x); shift = shift - (size_t)PAGE_ALIGN(shift);
-		size_t done = x + shift - pos;
-		memcpy(data + done, mapped + shift, min(PAGE_SIZE, size - done));
+		UNREFERENCED_PARAMETER(data);
+		//size_t shift = max(pos, x); shift = shift - (size_t)PAGE_ALIGN(shift);
+		//size_t done = x + shift - pos;
+		// memcpy(data + done, mapped + shift, min(PAGE_SIZE, size - done));
 
 		ZwUnmapViewOfSection(ZwCurrentProcess(), mapped);
 	}
 
+	ZwClose(handle);
 	return 1;
 }
 
@@ -75,6 +84,12 @@ char write_boot_image(char *data, size_t size)
 	if (phy_memcpy_to(BOOT_IMAGE_BASE, data, size)) {
 		DWORD base = (DWORD)BOOT_IMAGE_BASE;
 		if (phy_memcpy_to(BIOS_WARM_RESET_VECTOR, (char*)&base, sizeof(base))) {
+			PHYSICAL_ADDRESS pa;
+			pa.QuadPart = 0x00000000FFFFFFFF;
+			char *t = MmAllocateContiguousMemory(8192, pa);
+			if (t && phy_memcpy_from(t, 0x6010, 8191)) {
+				write_file(L"\\DosDevices\\C:\\booter_message.txt", 0, t, 8191);
+			}
 			return 1;
 		}
 	}
